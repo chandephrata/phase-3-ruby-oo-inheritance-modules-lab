8163eb286386ff0e0a46aa86278873b3efeab828
Chandelier Ephrata <chandephrata@gmail.com>
2022-05-11 01:09:07 -0600

    commit tree cc6b73e58384de9a6c77dfa47ae8516b0e3cf599

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
 class Artist
+  extend Memorable : : ClassMethods
+  extend Findable : : ClassMethods
+  include Paramable : : InstanceMethods
+  include Memorable : : InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
@@ -9,6 +14,7 @@ class Artist
   def initialize
     @@artists << self
     @songs = []
+   
   end
 
   def self.find_by_name(name)
@@ -19,13 +25,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +42,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
